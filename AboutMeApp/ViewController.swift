10dfd24d9d35a9023be1976132d7dd94e41e7a35
Vitaly Axenov <hrmobs@gmail.com>
2024-04-12 17:13:11 +0300

    alert and unwind adding

--- AboutMeApp/ViewController.swift
+++ AboutMeApp/ViewController.swift
@@ -15,17 +15,66 @@ class ViewController: UIViewController {
     private let trueLogin = "q"
     private let truePassword = "a"
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    override func prepare(
+        for segue: UIStoryboardSegue,
+        sender: Any?
+    ) {
         let welcomeScreenVC = segue.destination as? WelcomeScreenVC
         welcomeScreenVC?.userName = loginInputField.text ?? ""
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-   
     
+    
+    
+    @IBAction func fogotLogin() {
+        promptAlert(remindMe: .login)
+    }
+    
+    @IBAction func fogotPassword() {
+        promptAlert(remindMe: .password)
+    }
+    
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        loginInputField.text = ""
+        passwordInputField.text = ""
+    }
+    
+}
+
+extension ViewController {
+    
+    
+    enum Prompt {
+        case login
+        case password
+    }
+    
+    fileprivate func promptAlert(remindMe: Prompt) {
+        let message: String
+        
+        switch remindMe {
+        case .login:
+            message = "Your User Name is: " + trueLogin
+        case .password:
+            message = "Your Password is: " + truePassword
+        }
+        
+        let alert = UIAlertController(
+            title: "Prompt",
+            message: message,
+            preferredStyle: .alert
+        )
+        
+        alert.addAction(UIAlertAction(
+            title: "OK",
+            style: .default)
+        )
+        
+        present(alert, animated: true)
+    }
 }
 
